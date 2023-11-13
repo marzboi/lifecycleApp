ce615e3fff4b7a63faa1084be939a661f82e56eb
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-11-13 20:37:14 +0100

    Initial commit

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css'],
+})
+export class PriceComponent {}
---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,56 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'product-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+}
